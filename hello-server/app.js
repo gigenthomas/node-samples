30e612ef2447ea78dfcab3fb5226cef735d00dbf
gigent <gigenthomas@gmail.com>
2020-08-27 10:43:17 -0700

    simple module

--- hello-server/app.js
+++ hello-server/app.js
@@ -1,15 +1,13 @@
 const http = require('http');
-const rectange = require('./rectangle/rectangle');
+const Rectangle = require('./rectangle/rectangle');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-
-
 const server = http.createServer((req, res) => {
 
- //   rectange = new Rectangle( 10, 10);
-//    console.log(rectange.area());
+   rectange = new Rectangle( 10, 10);
+   console.log(rectange.area());
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
